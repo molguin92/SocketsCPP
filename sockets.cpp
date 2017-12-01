3a69e08971799da3dca078269bd1955bd53fed7e
Manuel Osvaldo Olguín <molguin@kth.se>
2017-12-01 14:04:26 -0500

    initial commit

---
+++ sockets.cpp
@@ -0,0 +1,191 @@
+//
+// Created by molguin on 2017-10-27.
+//
+
+#include <sys/un.h>
+#include "sockets.h"
+
+#define LOGURU_WITH_STREAMS 1
+
+#include <loguru/loguru.hpp>
+#include <zconf.h>
+
+namespace sockets
+{
+    UnixSocket::UnixSocket(std::string path)
+    : socket_path(std::move(path)),
+      socket_fd(socket(AF_UNIX, SOCK_SEQPACKET, 0))
+    {
+        CHECK_NE_S(-1, socket_fd) << "Could not open socket file descriptor. errno: " << strerror(errno);
+
+        auto* _addr = new sockaddr_un{};
+        _addr->sun_family = AF_UNIX;
+        strncpy(_addr->sun_path, socket_path.c_str(), sizeof(_addr->sun_path) - 1);
+        ISocket::setAddr((sockaddr*) _addr);
+
+        socketAPI.accept = accept;
+        socketAPI.connect = connect;
+        socketAPI.bind = bind;
+        socketAPI.listen = listen;
+
+        socketAPI.send = send;
+        socketAPI.recv = recv;
+
+        socketAPI.error_code = -1;
+    }
+
+    UnixSocket::~UnixSocket()
+    {
+        close(socket_fd);
+        if (ISocket::is_Bound())
+            unlink(socket_path.c_str());
+    }
+
+    Connection UnixSocket::Connect()
+    {
+        CHECK_NE_S(socketAPI.connect(socket_fd, ISocket::getAddr(), sizeof(sockaddr_un)), socketAPI.error_code)
+            << "Could not connect to socket: " << socket_path << ", errno: " << strerror(errno);
+
+        /*
+         * We need to copy the socket address to a new structure to pass to the connection since the destructor
+         * of the connection deletes its address - and we don't want to end up with a dangling pointer here,
+         * do we now?
+         */
+        auto _addr = ISocket::getAddr();
+        auto n_addr = (sockaddr*) malloc(sizeof(sockaddr));
+        memcpy(n_addr, _addr, sizeof(sockaddr));
+
+        return Connection(socket_fd, n_addr, socketAPI);
+    }
+
+    void UnixSocket::BindAndListen()
+    {
+        CHECK_NE_S(socketAPI.bind(socket_fd, ISocket::getAddr(), sizeof(sockaddr_un)), socketAPI.error_code)
+            << "Could not bind to socket: " << socket_path << ", errno: " << strerror(errno);
+
+        CHECK_NE_S(socketAPI.listen(socket_fd, MAX_CONNECTION_BACKLOG), socketAPI.error_code)
+            << "Could not listen on socket: " << socket_path << ", errno: " << strerror(errno);
+        ISocket::setBound();
+    }
+
+    Connection UnixSocket::AcceptConnection()
+    {
+        auto* peer_addr = (sockaddr*) (new sockaddr_un{});
+        socklen_t len = sizeof(sockaddr_un);
+        int connection_fd = socketAPI.accept(socket_fd, peer_addr, &len);
+        CHECK_NE_S(connection_fd, socketAPI.error_code)
+            << "Could not accept incoming connection on socket: "
+            << socket_path << ", errno: " << strerror(errno);
+
+        return Connection(connection_fd, peer_addr, socketAPI);
+    }
+
+
+    Connection::~Connection()
+    {
+        this->Close();
+        delete addr;
+    }
+
+    void Connection::sendInt32(int32_t var)
+    {
+        CHECK_S(open) << "Closed connection.";
+
+        uint32_t total_sent = 0;
+        ssize_t sent;
+
+        while (total_sent < sizeof(int32_t))
+        {
+            sent = socketAPI.send(fd, ((const char*) &var) + total_sent, sizeof(int32_t) - total_sent, 0);
+            CHECK_NE_S(sent, -1) << "Error when trying to send int32, errno: " << strerror(errno);
+            total_sent += sent;
+        }
+    }
+
+    void Connection::recvInt32(int32_t& var)
+    {
+        CHECK_S(open) << "Closed connection";
+
+        uint32_t total_received = 0;
+        ssize_t received;
+        while (total_received < sizeof(int32_t))
+        {
+            received = socketAPI.recv(fd, ((char*) &var) + total_received, sizeof(int32_t) - total_received, 0);
+            CHECK_NE_S(received, -1) << "Error when trying to receive int32, errno: " << strerror(errno);
+            total_received += received;
+        }
+    }
+
+    void Connection::sendBuffer(char* buf, size_t len)
+    {
+        CHECK_S(open) << "Closed connection";
+
+        size_t total_sent = 0;
+        ssize_t sent;
+        while (total_sent < len)
+        {
+            sent = socketAPI.send(fd, buf + total_sent, len - total_sent, 0);
+            CHECK_NE_S(sent, -1) << "Error when trying to send buffer, errno : " << strerror(errno);
+            total_sent += sent;
+        }
+    }
+
+    void Connection::recvBuffer(char* buf, size_t len)
+    {
+        CHECK_S(open) << "Closed connection";
+
+        size_t total_rcvd = 0;
+        ssize_t rcvd;
+        while (total_rcvd < len)
+        {
+            rcvd = socketAPI.recv(fd, buf + total_rcvd, len - total_rcvd, 0);
+            CHECK_NE_S(rcvd, -1) << "Error when trying to send buffer, errno : " << strerror(errno);
+            total_rcvd += rcvd;
+        }
+    }
+
+    void Connection::sendMessage(const google::protobuf::Message& msg)
+    {
+        // dump message into a buffer and send it
+        int len = msg.ByteSize();
+        auto buf = new char[len];
+
+        // TODO: error handling on the serialization
+        // TODO: counting size twice... maybe switch to using strings->chararrays?
+        msg.SerializeToArray(buf, len);
+
+        // first send length, then serialized message
+        sendInt32(len);
+        sendBuffer(buf, static_cast<size_t>(len));
+        delete buf;
+    }
+
+    void Connection::recvMessage(google::protobuf::Message& msg)
+    {
+        // first get length of message, then complete message
+        int32_t len = 0;
+        recvInt32(len);
+        auto* buf = new char[len];
+        recvBuffer(buf, static_cast<size_t>(len));
+
+        msg.ParseFromArray(buf, len);
+        delete buf;
+    }
+
+    void Connection::Close()
+    {
+        if (!open) return;
+        close(fd);
+        open = false;
+    }
+
+    void Connection::Open(const int fd, sockaddr* addr)
+    {
+        CHECK_S(!open) << "Connection already open.";
+        this->fd = fd;
+        this->addr = addr;
+        open = true;
+    }
+
+}
+
---
+++ sockets.h
@@ -0,0 +1,171 @@
+//
+// Created by molguin on 2017-10-27.
+//
+
+#ifndef MIGRATIONORCHESTRATOR_SOCKETS_H
+#define MIGRATIONORCHESTRATOR_SOCKETS_H
+
+#include <glob.h>
+#include <string>
+#include <google/protobuf/message.h>
+#include <functional>
+#include <utility>
+#include <sys/socket.h>
+
+#define MAX_CONNECTION_BACKLOG 128
+
+namespace sockets
+{
+/**
+ * @brief Defines an abstraction for the operations of sockets.
+ * This is to allow dependency injection in connections.
+ */
+    struct SocketAPI
+    {
+        int error_code;
+
+        std::function<int(int, const sockaddr*, socklen_t)> connect;
+        std::function<int(int, const sockaddr*, socklen_t)> bind;
+        std::function<int(int, sockaddr*, socklen_t*)> accept;
+        std::function<int(int, int)> listen;
+
+        std::function<ssize_t(int, const void*, size_t, int)> send;
+        std::function<ssize_t(int, void*, size_t, int)> recv;
+    };
+
+/**
+ * @brief Represents one end of a connection on top of a socket,
+ * and provides helper methods to send data to and receive data
+ * from the other end.
+ */
+    class Connection
+    {
+    private:
+        sockaddr* addr;
+        int fd;
+        bool open;
+
+        SocketAPI socketAPI;
+
+    public:
+        Connection() : open(false), fd(-1), addr(nullptr)
+        {}
+
+        Connection(const int fd, sockaddr* addr, SocketAPI api)
+        : fd(fd), addr(addr), socketAPI(std::move(api)), open(true)
+        {}
+
+        ~Connection();
+
+        void Open(int fd, sockaddr* addr);
+        void sendInt32(int32_t var);
+        void recvInt32(int32_t& var);
+        void sendBuffer(char* buf, size_t len);
+        void recvBuffer(char* buf, size_t len);
+        void Close();
+
+        /**
+         * @brief Sends a protobuf message through the connection.
+         * @param msg A Protocol Buffers message object.
+         */
+        void sendMessage(const google::protobuf::Message& msg);
+
+        /**
+         * @brief Receives a protobuf message through the connection.
+         * @param msg The Protocol Buffers message object where the incoming data should be stored.
+         */
+        void recvMessage(google::protobuf::Message& msg);
+
+        /**
+         * @brief Allows to override the socket input-output operations used by this
+         * connection object, for testing and dependency injection purposes.
+         * @param api
+         */
+        void setSocketAPI(SocketAPI api)
+        { socketAPI = std::move(api); }
+    };
+
+/**
+ * @brief Base class for all sockets. Defines a common interface for all sockets,
+ * unix, tcp, udp or udt to adhere to.
+ */
+    class ISocket
+    {
+    private:
+        bool is_bound;
+        sockaddr* addr{};
+
+    protected:
+
+        ISocket() : is_bound(false), addr(nullptr)
+        {}
+
+        virtual ~ISocket()
+        { delete addr; }
+
+        void setAddr(sockaddr* addr)
+        {
+            this->addr = addr;
+        }
+
+        void setBound()
+        {
+            is_bound = true;
+        }
+
+    public:
+        bool is_Bound() const
+        {
+            return is_bound;
+        }
+
+        const sockaddr* getAddr() const
+        {
+            return addr;
+        }
+
+
+        /**
+         * @brief Connect to the provided socket address.
+         * Calling this method implies using this socket as a client socket.
+         * @return A Connection object to send and receive data.
+         */
+        virtual Connection Connect() = 0;
+
+        /**
+         * @brief Bind and listen on the provided socket address, previous
+         * to an AcceptConnection call.
+         * Calling this method implies using this socket as a server socket.
+         */
+        virtual void BindAndListen() = 0;
+
+        /**
+         * @brief Accept an incoming connection from a client on the socket.
+         * Calling this method implies using this socket as a server socket.
+         * @return A Connection object to send and receive data.
+         */
+        virtual Connection AcceptConnection() = 0;
+    };
+
+/**
+ * @brief Abstraction of an Unix Domain Socket.
+ */
+    class UnixSocket : protected ISocket
+    {
+    private:
+        int socket_fd;
+        const std::string socket_path;
+        SocketAPI socketAPI;
+    public:
+
+        explicit UnixSocket(std::string path);
+        ~UnixSocket() override;
+
+        Connection Connect() override;
+        void BindAndListen() override;
+        Connection AcceptConnection() override;
+    };
+};
+
+
+#endif //MIGRATIONORCHESTRATOR_SOCKETS_H
